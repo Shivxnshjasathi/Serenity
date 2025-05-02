b35963c62e7bef4be384952a9101e2a281c62917
Pjcheats <90235875+Pjcheats@users.noreply.github.com>
2025-05-03 01:28:46 +0530

    added keystore

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:serenity/UI/splash.dart';
-import 'package:serenity/constants/const.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
