5fbf8898d11815cb7019560c7b99ce0ff8cb0b1d
Shivansh jasathi <116382647+Shivxnshjasathi@users.noreply.github.com>
2025-05-03 00:34:14 +0530

    Shree ram

--- lib/constants/const.dart
+++ lib/constants/const.dart
@@ -5,7 +5,10 @@ import 'package:google_fonts/google_fonts.dart';
 // const Color accentColor = Color(0xFFe4d4c6);
 const Color bgColor = Color(0xFF000000);
 const Color accentColor = Color(0xFFe4d4c6);
-String geminikey = "AIzaSyBQBXPyfwNJ0ckzNxa99jTbXJlT_Pe2gDI";
+
+String geminikey = "AIzaSyB0DbcbYw1xxESK3tfXVTfiPX5ah_G1XFI";
+
+// String geminikey = "AIzaSyBQBXPyfwNJ0ckzNxa99jTbXJlT_Pe2gDI";
 
 TextStyle primaryTextStyle = GoogleFonts.poppins(
   fontSize: 16, // You can adjust the size as needed
